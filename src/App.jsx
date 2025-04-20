d66748cf701580633d79ff9d0d0f6a5bc7dd5372
kapil9179 <kapilpandeyweb@gmail.com>
2025-04-20 23:16:51 +0530

    add payment history on your geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import "./App.css";
+// import "./App.css";
+import { PaymentHistory } from "./constants/tabledata";
 
 function App() {
   return (
     <>
-      {/* // this is la la */}
-      <div>Hello welcome to geolocation</div>
-      <div>this is workflow complete!!</div>
-      <div>this is access permissin resolve!!</div>
+      <div>
+        <table>
+          <thead>
+            <th>no</th>
+            <th>Amount</th>
+            <th>PaymentType</th>
+            <th>paymentDate</th>
+          </thead>
+          <tbody style={{ display: "flex" }}>
+            <tr>
+              {PaymentHistory.length > 0 ? (
+                PaymentHistory.map((pay, index) => (
+                  <>
+                    <td>{index + 1}</td>
+                    <td>{pay.paymentType}</td>
+                    <td>{pay.paymentDate}</td> 
+                    <td>{pay.amount}</td>
+                  </>
+                ))
+              ) : (
+                <p>
+                  Sorry Your Payment History Is Not Avilable Make Sure You Done
+                  Your Payment Autometically Show Your History Thank You So Much
+                  For You Comming
+                </p>
+              )}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
