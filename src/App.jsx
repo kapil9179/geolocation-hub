f5bdbfaf073f122eee7d88d7b53e86ad867f1b25
kapil9179 <kapilpandeyweb@gmail.com>
2025-04-20 22:46:00 +0530

    add permission

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       {/* // this is la la */}
       <div>Hello welcome to geolocation</div>
       <div>this is workflow complete!!</div>
+      <div>this is access permissin resolve!!</div>
     </>
   );
 }
